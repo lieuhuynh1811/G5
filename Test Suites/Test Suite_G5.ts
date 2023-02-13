382af900cf4ddab799e1582f6d4ac73a88f9eab1
lieuhuynh1811 <lieu.huynh@katalon.com>
2023-02-13 17:28:22 +0700

    G5 test script

---
+++ Test Suites/Test Suite_G5.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite_G5</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>e06317b2-707c-46cc-87aa-a78ae6d9bfe8</testSuiteGuid>
+   <testCaseLink>
+      <guid>b1bd78b1-89b6-4a3b-8a01-8897237b0420</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/G5 Test Case</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
